63846493252734fb6cdf077c93e8c6d578eb8e5d
jw <betrayers000@gmail.com>
2020-01-31 20:49:34 +0900

    [+] swift_memo start project

---
+++ Memo/Memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by 박정우 on 2020/01/22.
+//  Copyright © 2020 박정우. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
